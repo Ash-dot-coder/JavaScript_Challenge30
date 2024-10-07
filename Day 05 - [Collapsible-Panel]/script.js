1d0df174e439b03b69c1cbad7c5c8beeb808a210
Ayush Kohre <kohreaayush@gmail.com>
2024-10-07 18:56:14 +0530

    Day - 5 | Collapsible Panel

---
+++ Day 05 - [Collapsible-Panel]/script.js
@@ -0,0 +1,17 @@
+alert("JavaScript Challenge : Day-5")
+
+const panels = document.querySelectorAll('.panel');
+function toggleOpen() {
+    console.log("Hey!");
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active');
+    }    
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
