6d75a2b62d9c7095a536c85275e379bb34f6031e
Ayush Kohre <kohreaayush@gmail.com>
2024-10-26 09:50:49 +0530

    Day - 24 | Animated Nav

---
+++ Day 24 - [Animated-Nav]/script.js
@@ -0,0 +1,6 @@
+alert("JavaScript Challenge: Day-24");
+
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => nav.classList.toggle("active"));
